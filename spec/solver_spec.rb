9cc62ccea9d8052eef6ed9deb17f20c202c44dd9
youmari <youmari300@gmail.com>
2022-03-10 11:38:45 +0100

    write tests for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,26 +1,43 @@
 require './solver'
 
 describe Solver do
-  it "it should return the factorial of 5 which is 120" do
-    solver = Solver.new
-    
-    factorial = solver.factorial(5)
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  context "Testing for factorial" do
+    it "it should return the factorial of 5 which is 120" do
+    factorial = @solver.factorial(5)
 
     expect(factorial).to eql 120 
-  end
+    end
 
-  it "it should return an raise an error" do
-    solver = Solver.new
-    expect {factorial = solver.factorial(-1)}.to raise_error(Exception,'negative numbers not allowed')
-  end
+    it "it should return an raise an error" do
+      expect {@solver.factorial(-1)}.to raise_error(Exception,'negative numbers not allowed')
+    end
 
-  it "it should return an nil" do
-    solver = Solver.new
+    it "it should return an nil" do
+      factorial = @solver.factorial(nil)
 
-    factorial = solver.factorial(nil)
-
-    expect(factorial).to be nil
+      expect(factorial).to be nil
+    end
   end
   
+  context "Testing for reverse method" do
+    it "should return a reverse string" do
+      reverse = @solver.reverse('hello')
+
+      expect(reverse).to eql 'olleh' 
+    end
+
+    it "should return an Exception if input is not a string" do
+      reverse = @solver.reverse(nil)
+      reverse1 = @solver.reverse(89)
+
+      expect { @solver.reverse(nil)}.to raise_error(Exception,'input is not a string')
+      expect { @solver.reverse(89) }.to raise_error(Exception,'input is not a string')
+    end
+    
+  end
   
 end
